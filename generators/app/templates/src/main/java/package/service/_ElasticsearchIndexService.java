c8543b3b5e6dfd9d7f99df3756abd8f438707774
Gerald Humphries <geraldhumphries@gmail.com>
2016-02-11 13:01:50 -0500

    reindex each entity in its own transaction
    fix #10

--- generators/app/templates/src/main/java/package/service/_ElasticsearchIndexService.java
+++ generators/app/templates/src/main/java/package/service/_ElasticsearchIndexService.java
@@ -7,14 +7,16 @@ import <%=packageName%>.repository.search.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.inject.Inject;
+import java.io.Serializable;
 
 @Service
-@Transactional
 public class ElasticsearchIndexService {
 
     private final Logger log = LoggerFactory.getLogger(ElasticsearchIndexService.class);
@@ -44,19 +46,20 @@ public class ElasticsearchIndexService {
         <%_ entityFiles.forEach(function (file) {
             var entity = file.split('.json')[0];
             var entityLowerCase = entity.charAt(0).toLowerCase() + entity.slice(1); _%>
-        elasticsearchTemplate.deleteIndex(<%=entity%>.class);
-        if (<%=entityLowerCase%>Repository.count() > 0) {
-            <%=entityLowerCase%>SearchRepository.save(<%=entityLowerCase%>Repository.findAll());
-        }
-        log.info("Elasticsearch: Indexed all <%=entityLowerCase%>s");
-
+        reindexForClass(<%=entity%>.class, <%=entityLowerCase%>Repository, <%=entityLowerCase%>SearchRepository);
         <%_ }); _%>
-        elasticsearchTemplate.deleteIndex(User.class);
-        if (userRepository.count() > 0) {
-            userSearchRepository.save(userRepository.findAll());
-        }
-        log.info("Elasticsearch: Indexed all users");
+        reindexForClass(User.class, userRepository, userSearchRepository);
 
         log.info("Elasticsearch: Successfully performed reindexing");
+    }
+
+    @Transactional
+    private <T extends Serializable> void reindexForClass(Class<T> entityClass, JpaRepository<T, Long> jpaRepository,
+                                                          ElasticsearchRepository<T, Long> elasticsearchRepository) {
+        elasticsearchTemplate.deleteIndex(entityClass);
+        if (jpaRepository.count() > 0) {
+            elasticsearchRepository.save(jpaRepository.findAll());
+        }
+        log.info("Elasticsearch: Indexed all rows for " + entityClass.getSimpleName());
     }
 }
