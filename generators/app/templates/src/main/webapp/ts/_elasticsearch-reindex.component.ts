a64f6bdd197ebf574b7966aaad188deb3647e77c
Alexander Volf <job-github@wolf-a.ru>
2018-05-16 18:44:30 +0300

    Update _elasticsearch-reindex.component.ts

--- generators/app/templates/src/main/webapp/ts/_elasticsearch-reindex.component.ts
+++ generators/app/templates/src/main/webapp/ts/_elasticsearch-reindex.component.ts
@@ -5,6 +5,7 @@ import { JhiLanguageService } from 'ng-jhipster';
 <%_ } _%>
 
 import { ElasticsearchReindexModalComponent } from './elasticsearch-reindex-modal.component';
+import { ElasticsearchReindexSelectedModalComponent } from './elasticsearch-reindex-selected-modal.component';
 
 @Component({
     selector: 'jhi-elasticsearch-reindex',
@@ -12,16 +13,51 @@ import { ElasticsearchReindexModalComponent } from './elasticsearch-reindex-moda
 })
 export class ElasticsearchReindexComponent {
 
+    entities: string[];
+    reindexType: string;
+    checks: { [key: string]: boolean } = {};
+
     constructor(
         <%_ if (enableTranslation && requiresSetLocation) { _%>
         private jhiLanguageService: JhiLanguageService,
         <%_ } _%>
         private modalService: NgbModal
-    )<%_ if (enableTranslation && requiresSetLocation) { %> {
+    ) {
+      <%_ if (enableTranslation && requiresSetLocation) { %>
         this.jhiLanguageService.setLocations(['elasticsearch-reindex']);
-    }<%_ } else { %> { }<%_ } %>
+      <%_ } _%>
+        this.reindexType = 'all';
+        this.entities = [
+    <%_ if (applicationType === 'monolith' || applicationType === 'microservice') {
+            entityFiles.forEach(function (file) {
+                var entity = file.split('.json')[0]; _%>
+                '<%=entity%>',
+    <%_     });
+        } 
+    _%>          
+                'User'
+        ];        
+    }
+
+    doReindex() {
+        if (this.reindexType === 'all') {
+            this.showConfirm();
+        } else {
+            this.showConfirmSelected();
+        }
+    }
 
     showConfirm() {
         this.modalService.open(ElasticsearchReindexModalComponent);
     }
+
+    showConfirmSelected() {
+        const activeModal = this.modalService.open(ElasticsearchReindexSelectedModalComponent);
+        const checks = this.checks;
+        const reindexList = this.entities.filter(function(name) {
+            return checks[name];
+        });
+        activeModal.componentInstance.entities = reindexList;
+    }
+
 }
