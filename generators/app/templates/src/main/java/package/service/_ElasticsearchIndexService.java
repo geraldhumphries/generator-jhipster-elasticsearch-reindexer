660f7bae316a32b97441f2413e535958abd80ce3
Alexander Volf <job-github@wolf-a.ru>
2018-05-16 09:06:52 +0300

    Update _ElasticsearchIndexService.java

--- generators/app/templates/src/main/java/package/service/_ElasticsearchIndexService.java
+++ generators/app/templates/src/main/java/package/service/_ElasticsearchIndexService.java
@@ -204,7 +204,7 @@ public class ElasticsearchIndexService {
                 Pageable page = new PageRequest(i, size);
             <%_ } else if (jhipsterMajorVersion > 4) { _%>
                 Pageable page = PageRequest.of(i, size);
-            <%_ } _ %>
+            <%_ } _%>
                 log.info("Indexing page {} of {}, size {}", i, jpaRepository.count() / size, size);
                 Page<T> results = jpaRepository.findAll(page);
                 results.map(result -> {
